b60564f092f8482dc7edbcc04c6092294fa0dc7a
Anirudh171202 <anirudh.sathiya@gmail.com>
2020-03-28 02:05:36 +0530

    works

--- Data/createJ.py
+++ Data/createJ.py
@@ -1,51 +1,49 @@
 import json
 import os
-with open("data.txt","r") as f :
-	# try:
-	x = f.read()
-	print(len(x))
-	jsonData = {}
-	if len(x) != 0 :
-		jsonData = eval(x)
-		
-	f.close()
-	#windows ppl make forward slash to backward
-	os.remove(os.getcwd()+"/data.txt")
-	print(jsonData)
+
+with open(f"{name}.json","w+") as f :
 	
+	x = f.read()
+	if not x:
+		jsonData = {}
 
-
-		
+	else:	
+		jsonData = json.load(f)		
+	print("just got json: ", jsonData)
 	
 	while True:
+		
+			
+			
+				
 		mode = input("Enter i to insert d to delete v to view e to exit")
 		if mode == "i":
 			question = input("Enter qs: ")
 			answer = input("Enter ans: ")
 			source = input("Enter source :")
-			jsonData[question] = {"answer":answer, "source":source}
+			jsonData[question] = {"answer":answer,"source":source}
 			
 
 		elif mode == "d":
+			
 			word = input("Enter first word of question to delete")
+			ans = None
 			for key in jsonData.keys() :
 				if key.split(' ')[0] == word:
-					del jsonData[key]
+					ans = key
+			if not ans:
+				del jsonData[ans]
 			json.dump(jsonData, f)
 		elif mode == "v":
+			print(jsonData)
 			print(json.dumps(jsonData, indent=4))
 
 		elif mode == "e":
-			# os.remove(os.getpwd()+"/data.json")
-			with open("data.txt", "w+") as ff:
-				ff.write(str(jsonData))
-
 			break
 		else:
 			print("Pls try again....")
-	# except:
-	# 	with open("error.json", "w+") as ff:
-	# 		json.dump(jsonData, ff)
+
+		
 
 
 	print("Bye")
