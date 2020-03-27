723459bc0bfa827d69f6c76e530c91661ac1413d
Anirudh171202 <anirudh.sathiya@gmail.com>
2020-03-28 02:20:19 +0530

    a

--- Data/createjson.py
+++ Data/createjson.py
@@ -1,7 +1,21 @@
 import json
 import os
+
+l = []
+directory = os.getcwd()
+for file in os.listdir(directory):
+	if file.endswith(".txt"):
+		l.append(file)
+print(l)
+if "data.txt" not in l:
+	with open("data.txt","w+") as w:
+		w.write("{}")
+		w.close()
+
+
 with open("data.txt","r") as f :
-	# try:
+	
+	
 	x = f.read()
 	print(len(x))
 	jsonData = {}
@@ -9,7 +23,7 @@ with open("data.txt","r") as f :
 		jsonData = eval(x)
 		
 	f.close()
-	#windows ppl make forward slash to backward
+	#windows ppl make /data.txt to \\data.txt
 	os.remove(os.getcwd()+"/data.txt")
 	print(jsonData)
 	
@@ -28,10 +42,14 @@ with open("data.txt","r") as f :
 
 		elif mode == "d":
 			word = input("Enter first word of question to delete")
+			ans = None
 			for key in jsonData.keys() :
-				if key.split(' ')[0] == word:
-					del jsonData[key]
-			json.dump(jsonData, f)
+				if len(key)>=1 and key.split(' ')[0] == word:
+					ans = key
+			del jsonData[ans]
+			
+
+			
 		elif mode == "v":
 			print(json.dumps(jsonData, indent=4))
 
