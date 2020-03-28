6b8a3671c5eff8b02700b6714acb63d824535a33
Lutetium-Vanadium <luv.s7000@gmail.com>
2020-03-28 11:29:07 +0530

    db changes

--- database/make_db.py
+++ database/make_db.py
@@ -1,20 +1,24 @@
-import GetOldTweets3 as got
+# import GetOldTweets3 as got
 
-def dumps(lst):
-    print("[")
-    for i in lst:
-        print(f"  {str(i[0]), i[1]},")
-    print("]")
+# def dumps(lst):
+#     print("[")
+#     for i in lst:
+#         print(f"  {str(i[0]), i[1]},")
+#     print("]")
 
-username = 'WHO IS COVID'
-count = 10 # Creation of query object
-tweetCriteria = got.manager.TweetCriteria().setUsername(username)\
-                                        .setMaxTweets(count)
+# username = 'WHO IS COVID'
+# count = 10 # Creation of query object
+# tweetCriteria = got.manager.TweetCriteria().setUsername(username)\
+#                                         .setMaxTweets(count)
 
-print("Ani")
-# Creation of list that contains all tweets
-tweets = got.manager.TweetManager.getTweets(tweetCriteria) # Creating list of chosen tweet data
-print("Vishnu Anand")
-user_tweets = [[tweet.date, tweet.text] for tweet in tweets]
+# print("Ani")
+# # Creation of list that contains all tweets
+# tweets = got.manager.TweetManager.getTweets(tweetCriteria) # Creating list of chosen tweet data
+# print("Vishnu Anand")
+# user_tweets = [[tweet.date, tweet.text] for tweet in tweets]
 
-print(dumps(user_tweets))
+# print(dumps(user_tweets))
+
+import pandas as pd
+
+df = pd.read_csv("WHOTwitterData.csv")
