7fd30a327b291bba8193fa24789e7462558ee26f
VrishabKrishna <krishnavrishab@gmail.com>
2020-03-29 00:35:28 +0530

    chore: rm slack.py

--- slack.py
+++
@@ -1,9 +0,0 @@
-import os
-import slack
-slack_token = os.environ["xoxb-1021109435873-1032579478213-LfqXw0tPhWZuvqrsXVtMF0Ea"]
-client = slack.WebClient(token=slack_token)
-
-client.chat_postMessage(
-  channel="U010WV4JH0C",
-  text="Hello from your app! :tada:"
-)
